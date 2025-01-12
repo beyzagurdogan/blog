2c8549f5c819c9292a499bcd060de1ee474db47d
beyzagurdogan <beyzae75@gmail.com>
2025-01-12 23:09:10 +0100

    change on homepage

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog_home, name='home'),  # Blog ana sayfasına yönlendirme
+    path('', views.blog_home, name='blog_home'),  # Blog ana sayfasına yönlendirme
+    path('post/<int:blog_id>/', views.blog_detail, name='blog_detail'),  # Detay sayfası
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
+
 
 def blog_home(request):
     posts = Blog.objects.all()  # Tüm blog yazılarını al
     return render(request, 'blog/home.html', {'posts': posts})
+
+
+
+def blog_detail(request, blog_id):
+    post = get_object_or_404(Blog, id=blog_id)  # Yazıyı al veya 404 döndür
+    return render(request, 'blog/detail.html', {'post': post})
